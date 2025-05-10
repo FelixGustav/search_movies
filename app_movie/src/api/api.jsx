89d821398593b153b2fb569d39c3d28bd1a0b6c1
FelixGustav <guto.felix2003@gmail.com>
2025-05-09 23:28:35 -0300

    implementation of the main logic

--- app_movie/src/api/api.jsx
+++ app_movie/src/api/api.jsx
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
-const API_KEY = '9d21352e' // Substitua pela sua chave da OMDB
-const API_URL = 'https://wwwhttp://www.omdbapi.com/?i=tt38 96198&apikey=9d21352e.omdbapi.com/'
-
 export const fetchMovies = async (query) => {
   const response = await axios.get(API_URL, {
     params: {
